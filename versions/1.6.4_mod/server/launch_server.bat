d117fb6da454fd576b7b2bfc53c0fa8fd177afbb
oseaniic <ducesndustftw@gmail.com>
2025-07-01 21:11:30 -0500

    magic

--- versions/1.6.4_mod/server/launch_server.bat
+++ versions/1.6.4_mod/server/launch_server.bat
@@ -1,4 +1,4 @@
 @ECHO OFF
 SET BINDIR=%~dp0
 CD /D "%BINDIR%"
-"%ProgramFiles%\Java\jre1.8.0_202\bin\java.exe" -Xmx8192M -Xms8192M -jar minecraftforge-universal-1.6.4-9.11.1.1345.jar
+"%ProgramFiles%\Java\jre1.8.0_202\bin\java.exe" -Xmx9999M -Xms9999M -jar minecraftforge-universal-1.6.4-9.11.1.1345.jar
